b3a7f589ebb0716d1a3731576c5c6c34d51d6b11
Andrew5205 <andrewchung11@gmail.com>
2020-05-21 12:08:06 -0700

    Add 36_Rule3.js

---
+++ Sec3/36_Rule3.js
@@ -0,0 +1,14 @@
+
+function compressBoxesTwice(boxes, boxes2) {
+    boxes.forEach( function(boxes) {
+        console.log(boxes);
+    });
+
+    boxes2.forEach( function(boxes) {
+        console.log(boxes2);
+    })
+}
+
+// boxes.length != boxes2.length
+
+// O(a+b)
