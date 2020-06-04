2c52cab5d4f72ec1373bb2e72db6dc8004261e08
Andrew5205 <andrewchung11@gmail.com>
2020-06-04 15:00:56 -0700

    Add on BST 129_implementing_Binary_Search_Tree.js

---
+++ Sec10_DS_Tree/129_implementing_Binary_Search_tree.js
@@ -0,0 +1,95 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class BinarySearchTree {
+    constructor() {
+        // BST start point root -> null
+        this.root = null;
+    }
+
+    // insert 
+    // 1. chech empty or not 
+    //      if empty, root pointer = newNode 
+    // 2. set a runner: currentNode = this.root
+    // 3. not empty, use while(true) to loop 
+            // two option value < or > currentNode
+    // 4. within each option, check currentNode has child or not
+            // if no child, assign value, and return this
+            // if child, move to next level
+    // 5. move to the next level (end point for while loop)
+    
+    insert(value) {
+        // create a newNode, so we have a reference to it 
+        // create a new Node class, instantiating it with a value
+        const newNode = new Node(value);
+        if ( this.root == null) {
+            this.root = newNode;
+        } else {
+            // if the root exisit 
+            // get another reference point starting at this.root 
+            let currentNode = this.root;
+
+            // trick to build tree 
+            // dont know exactlly how long we gonna traverse 
+            // while(true) means keep moving 
+            // make sure having a end point
+            while( true ) {
+                if ( value < currentNode.value ) {
+                    if ( !currentNode.left ) {
+                        currentNode.left = newNode;
+                        // end point, exist while loop 
+                        return this;
+                    }
+                    // if on the left already have a node, move to the next left
+                    currentNode = currentNode.left;
+                } else {
+                    if ( !currentNode.right ) {
+                        currentNode.right = newNode;
+                        // end point, exist while loop 
+                        return this;
+                    }
+                    // if on the right already have a node, move to the next right
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+    }
+
+
+}
+
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+
+
+
+JSON.stringify(traverse(tree.root));
+console.log(JSON.stringify(traverse(tree.root)));
+console.log(tree);
+
+
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+    const tree = { value: node.value };
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+    return tree;
+}
+
