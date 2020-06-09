808e0d38dbbb1cf0a8df618f71f2e9fd42da6ad9
Andrew5205 <andrewchung11@gmail.com>
2020-06-09 08:06:10 -0700

    Add on 136_Binary_Heap.js

---
+++ Sec10_DS_Tree/136_Binary_Heap.js
@@ -0,0 +1,19 @@
+
+// binary heap - left/ right can be any value but greater/ less then the parent
+// missing condition
+
+// https://visualgo.net/en/heap?slide=1
+
+// max heap  vs  min heap 
+
+// max heap: max on top (root)
+// min heap: min on top (root)
+
+
+// lookup O(n)
+// insert O(long n)
+// delete O(long n)
+
+
+
+// great in doing comparing
